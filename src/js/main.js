a636b9f277cd5cbdd4b0d4b3772a0e17a0f8ef18
Manan <manan19jain@gmail.com>
2023-02-18 05:42:04 +0530

    Basic design done: accept input, style, validate

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+const button = document.querySelector('#button');
+button.addEventListener('click', getUserInput);
+
+function getUserInput() {
+    var elevators = document.querySelector('#text-elevators').value;
+    var floors = document.querySelector('#text-floors').value;
+
+    //variable that only supports unsigned integers
+    var { elevators, floors } = checkValidInput(elevators, floors);
+
+    console.log("elevators = " + elevators);
+    console.log("floors = " + floors);
+
+}
+
+function checkValidInput(elevators, floors) {
+    var elevatorsInt = parseInt(elevators, 10);
+    var floorsInt = parseInt(floors, 10);
+
+    // check for invalid inputs
+    if (elevatorsInt < 0) {
+        alert("Please enter a positive number of elevators");
+    }
+    if (floorsInt < 0) {
+        alert("Please enter a positive number of floors");
+    }
+    if (elevatorsInt % 1 != 0) {
+        alert("Please enter the number of elevators as an integer");
+    }
+    if (floorsInt % 1 != 0) {
+        alert("Please enter the number of floors as an integer");
+    }
+    return { elevators, floors };
+}
